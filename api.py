40dd99d7abe1cd28ee8583be0e0f6751888f9672
shailyanand <shaily.higins@gmail.com>
2025-01-31 13:01:40 +0530

    api updates

--- api.py
+++ api.py
@@ -1,11 +1,44 @@
-###Name : Shaily anand
-### started on:04-07-2021
-### updated by: NA
+"""
+This is a new project for practice FastAPI
+"""
+from fastapi import FastAPI # import fastapi
+from pydantic import BaseModel # import base model
 
-def function():
-    print("New Project for practice")
-    return None
+app = FastAPI() #create fastapi instance
+
+list_items = []
 
 
+@app.get("/") # define path operation decorator
+def hello_world():
+    """
+    Print hello world
+    """
+    
+    return {"Hello": "World"}
 
-function()
+@app.get("/item/{item_id}") # define path operation decorator
+def hello_world(item_id:int):
+    """
+    path parameter
+    """
+    
+    return {"item_id": item_id}
+
+
+@app.post("/item") # define query operation decorator
+def collect_items(item_name:str):
+    """
+    query parameter
+    """
+    list_items.append(item_name)
+    return list_items
+
+@app.get("/items/{item_id}") # define path operation decorator
+def represnt_items(item_id:int) -> str:
+    """
+    path parameter-2
+    Have all the items be displayed as per item number inthe list
+    """
+    return list_items[item_id]
+
